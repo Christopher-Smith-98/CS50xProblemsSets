ea06340356ff2019c1510fdf5b0e7d432c156f8e
Christopher <bcsmithboys1@gmail.com>
2022-02-20 15:22:41 -0600

    Add project files.

---
+++ CS50xProblemsSets.cpp
@@ -0,0 +1,103 @@
+//Problem set 1/// C
+//CASH//////////////////////////////////////////////////////////
+
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Input how many cents: ");
+    } while (cents < 0);
+
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+    int quarters = cents / 25;
+    return quarters;
+}
+
+int calculate_dimes(int cents)
+{
+    int dimes = cents / 10;
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+    int nickels = cents / 5;
+    return nickels;
+}
+
+int calculate_pennies(int cents)
+{
+    int pennies = cents / 1;
+    return pennies;
+}
+
+//MARIO/////////////////////////////////////////////////////
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int k = height - i; k > 1; k--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
