9c92d8600b60191d6f8807509a0298d6c9ed9abc
Christopher <bcsmithboys1@gmail.com>
2022-03-08 14:27:21 -0600

    finished readability

--- CS50xProblemsSets.cpp
+++ CS50xProblemsSets.cpp
@@ -108,19 +108,49 @@ int main(void)
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main(void)
 {
     string text = get_string("Text: ");
     printf("%s\n", text);
-    int words = 0;
-    int sentences = 0;
-    int chars = 0;
+    int W = 1;
+    int S = 0;
+    int L = 0;
 
     for (int i = 0; i < strlen(text); i++)
     {
         if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
-            chars++;
+            L++;
     }
-    printf("%i letters\n", chars);
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == ' ')
+            W++;
+    }
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            S++;
+    }
+
+    float calculation = (0.0588 * L / W * 100) - (0.296 * S / W * 100) - 15.8;
+    int index = round(calculation);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+        return 0;
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+        return 0;
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
