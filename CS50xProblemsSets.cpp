ce29241343522c647a8e3c5e7d90d21ad35c32ef
Christopher <bcsmithboys1@gmail.com>
2022-03-07 21:52:41 -0600

    Start of readability

--- CS50xProblemsSets.cpp
+++ CS50xProblemsSets.cpp
@@ -101,3 +101,26 @@ int main(void)
         printf("\n");
     }
 }
+
+
+//READABILITY///////////////////////////////////////////////////
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    printf("%s\n", text);
+    int words = 0;
+    int sentences = 0;
+    int chars = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+            chars++;
+    }
+    printf("%i letters\n", chars);
+}
