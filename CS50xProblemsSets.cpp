409e590ddbc30770a3bc0b6035601a5b87b0cc87
Christopher <bcsmithboys1@gmail.com>
2022-03-09 16:19:12 -0600

    added caesar to problem sets 2

--- CS50xProblemsSets.cpp
+++ CS50xProblemsSets.cpp
@@ -1,4 +1,4 @@
-//Problem set 1/// C
+//Problem set 1/// CS50
 //CASH//////////////////////////////////////////////////////////
 
 #include <cs50.h>
@@ -102,7 +102,7 @@ int main(void)
     }
 }
 
-
+//Problem Set 2// CS50
 //READABILITY///////////////////////////////////////////////////
 
 #include <cs50.h>
@@ -153,4 +153,53 @@ int main(void)
     {
         printf("Grade %i\n", index);
     }
+}
+
+//Caesar///////////////////////////////////////////////////////////////////////////
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caeser key\n");
+        return 1;
+    }
+
+    string key = argv[1];
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caeser key\n");
+            return 1;
+        }
+    }
+
+    string plaintext = get_string("plaintext: ");
+
+    int k = atoi(key);
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 65) + k) % 26) + 65);
+        }
+        else if (islower(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 97) + k) % 26) + 97);
+        }
+        else
+        {
+            printf("%c", (plaintext[i]));
+        }
+    }
+    printf("\n");
 }
