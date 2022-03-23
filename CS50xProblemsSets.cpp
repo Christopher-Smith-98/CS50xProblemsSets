4f40393ab8cd121c8bdd3ddd1bcd399edf3eb967
Christopher <bcsmithboys1@gmail.com>
2022-03-23 15:20:32 -0600

    started problem set 4 filters, greyscale.

--- CS50xProblemsSets.cpp
+++ CS50xProblemsSets.cpp
@@ -540,3 +540,23 @@ void eliminate(int min)
     }
     return;
 }
+
+//Problem set 4//////////////////////////////////////////////////////////////////////
+// Filter //(Just putting the code I had to write, not the whole thing.)
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float red = image[i][j].rgbtRed;
+            float green = image[i][j].rgbtGreen;
+            float blue = image[i][j].rgbtBlue;
+            int average = round((red + green + blue) / 3.0);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
