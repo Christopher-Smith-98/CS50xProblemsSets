9d7d21b40f0c8c8e8a309204f7e7c3bf5c7e893e
Christopher <bcsmithboys1@gmail.com>
2022-03-27 17:45:35 -0600

    added recover to problem set 4

--- CS50xProblemsSets.cpp
+++ CS50xProblemsSets.cpp
@@ -612,3 +612,64 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     } 
     return;
 }
+
+//Problem set 4 // PART 2 /////////////////////////////////////////
+
+// RECOVER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    //Open the file to read
+    FILE* input_file = fopen(argv[1], "r");
+
+    //Check if file is valid
+    if (input_file == NULL)
+    {
+        printf("Could not open the file");
+        return 2;
+    }
+
+    unsigned char buffer[512];
+
+    int count_image = 0;
+
+    FILE* output_file = NULL;
+
+    char* filename = malloc(8 * (sizeof(char)));
+
+    //Reading the blocks of 512 bytes
+    while (fread(buffer, sizeof(char), 512, input_file))
+    {
+        if (buffer[0] == 0xff & buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            sprintf(filename, "%03i.jpg", count_image);
+
+            output_file = fopen(filename, "w");
+
+            count_image++;
+        }
+        if (output_file != NULL)
+        {
+            fwrite(buffer, sizeof(char), 512, output_file);
+        }
+    }
+    free(filename);
+    fclose(output_file);
+    fclose(input_file);
+
+    return 0;
+}
+
+
